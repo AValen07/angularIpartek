175f9a35759c047ed4d4be8315b85d69b3885d60
AnderSolana <valearangu@hotmail.com>
2020-01-27 13:58:20 +0100

    navbar y componentes nuevos

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(private http: HttpClient) {
+    console.trace('PokemonService constructor');
+
+  }
+
+  getPokemon(){
+    
+    let url='https://pokeapi.co/api/v2/pokemon/1/';
+    console.trace('Ha llamado al metodo getPokemon '+url);
+    return this.http.get(url);
+  }
+}
